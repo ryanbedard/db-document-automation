85e55c09b48bf3d5411a02db37d5ada3bf9365af
Ryan Bedard <rbedard90@gmail.com>
2018-10-26 10:25:36 -0400

    Add files via upload
    Added a console to control the program and altered it to be more flexible. Hardcoded values have been removed.

--- app.py
+++ app.py
@@ -8,40 +8,35 @@ import os
 import pandas
 
 
-if __name__ == "__main__":
-    t = 0
+def instructions():
+    # print menu options
+    print('''
+Server and Database Program
+========
+Commands:
+  help - Displays this menu again
+  view - Displays connection strings for all databases
+  edit - Brings up options to edit an existing connection string
+  add - Adds a new connection string
+  remove - Removes a selected connection string
+  run - Runs the program
+''')
+
+
+def generate_documents():
     for connection in settings.connection_string:
         db_test = Connection(connection, settings.connection_args)
         result = db_test.executor.execute(("SELECT database_name = DB_NAME(database_id) , "
-                                       "total_size_gb = CAST(SUM(size) * 8. / 1024/1024 AS DECIMAL(8,3)) "
-                                       "FROM sys.master_files WITH(NOWAIT) "
-                                       "where DB_NAME(database_id) "
-                                       "NOT IN ('AdventureWorks2012_Data','master','tempdb','model','msdb','ReportServer','ReportServerTempDB','Northwind') "
-                                       "AND DB_NAME(database_id) NOT LIKE 'z%' GROUP BY database_id ORDER BY database_name"))
+                                           "total_size_gb = CAST(SUM(size) * 8. / 1024/1024 AS DECIMAL(8,3)) "
+                                           "FROM sys.master_files WITH(NOWAIT) "
+                                           "where DB_NAME(database_id) "
+                                           "NOT IN ('AdventureWorks2012_Data','master','tempdb','model','msdb','ReportServer','ReportServerTempDB','Northwind') "
+                                           "AND DB_NAME(database_id) NOT LIKE 'z%' GROUP BY database_id ORDER BY database_name"))
         r = list(result)
-        if t == 0:
-            old_file = 'DRDEV-D01.csv'
-            new_file = 'change.csv'
-            xls_file = 'DRDEV-D01.xls'
-        elif t == 1:
-            old_file = 'DRDEV-T01.csv'
-            new_file = 'change1.csv'
-            xls_file = 'DRDEV-T01.xls'
-        elif t == 2:
-            old_file = 'DRDEV-M01.csv'
-            new_file = 'change2.csv'
-            xls_file = 'DRDEV-M01.xls'
-        elif t == 3:
-            old_file = 'DRDEV-N01.csv'
-            new_file = 'change3.csv'
-            xls_file = 'DRDEV-N01.xls'
-        elif t == 4:
-            old_file = 'DRDEV11.csv'
-            new_file = 'change4.csv'
-            xls_file = 'DRDEV11.xls'
-        else:
-            pass
-        t += 1
+        server_name = connection[connection.find('@') + 1:connection.find('.')].upper()
+        old_file = server_name + '.csv'
+        new_file = 'change.csv'
+        xls_file = server_name + '.xls'
         with open(old_file, 'r', newline='') as csvinput:
             with open(new_file, 'w', newline='') as csvoutput:
                 writer = csv.writer(csvoutput)
@@ -66,7 +61,7 @@ if __name__ == "__main__":
                             successful = True
                         elif row[0] == r[i][0]:
                             x = row[-2]
-                            y = (r[i][1]-Decimal(x.strip(' "')))
+                            y = (r[i][1] - Decimal(x.strip(' "')))
                             writer.writerow(row[:-3] + [row[-2]] + [r[i][1]] + [y])
                             i += 1
                             successful = True
@@ -75,7 +70,7 @@ if __name__ == "__main__":
                             successful = True
                         else:
                             n = len(row) - 3
-                            writer.writerow([r[i][0]] + ['']*n + [r[i][1]] + ['0'])
+                            writer.writerow([r[i][0]] + [''] * n + [r[i][1]] + ['0'])
                             i += 1
         pandas.read_csv(new_file).to_excel(xls_file, index=False)
         copyfile(old_file, 'old_' + old_file)
@@ -83,3 +78,57 @@ if __name__ == "__main__":
         if os.path.exists(new_file):
             os.remove(new_file)
     db_test.executor.close()
+
+
+if __name__ == "__main__":
+    instructions()
+    pastSelection = ''
+    newSettings = settings.connection_string
+    while True:
+        selection = ''
+        while selection == '':
+            selection = input('>')
+
+        selection = selection.lower().strip()
+
+        if pastSelection == 'remove':
+            pastSelection = ''
+            if selection == 'exit':
+                break
+            elif len(newSettings) > int(selection) >= 0:
+                del newSettings[int(selection)]
+                with open('connections.txt', "w") as newfile:
+                    newfile.writelines(newSettings)
+                print('Entry removed')
+            else:
+                print('Invalid Entry')
+                break
+        elif pastSelection == 'add':
+            pastSelection = ''
+            if selection == 'exit':
+                break
+            else:
+                with open('connections.txt', "w") as newfile:
+                    newSettings.append('\n' + selection)
+                    newfile.writelines(newSettings)
+                print('Entry added')
+        else:
+            if selection == 'view':
+                print(''.join(newSettings))
+            elif selection == 'remove':
+                pastSelection = selection
+                print('Select an option to remove or type exit to return to the main menu:')
+                t = 0
+                for connection in newSettings:
+                    connection = connection.rstrip('\n')
+                    print(str(t) + ' ' + connection),
+                    t += 1
+            elif selection == 'add':
+                pastSelection = selection
+                print('Enter a connection string or type exit to return to the main menu:')
+            elif selection == 'run':
+                generate_documents()
+                print('''Documents have been generated
+                Copy the xls files and upload them into Confluence''')
+            elif selection == 'help':
+                instructions()
